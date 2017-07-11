3033ca273b514a1edf43ace0ee5af8f66526224f
Kristin Caron <krissycaron@gmail.com>
2017-07-11 11:04:32 -0500

    finished excersie

--- Program.cs
+++ Program.cs
@@ -12,15 +12,21 @@ namespace list
             planetList.Add("Jupiter");
             planetList.Add("Saturn");
         
-            List<string> planetList2 = new List<string>(){"neptune", "uranus"};
+            List<string> planetList2 = new List<string>(){"Neptune", "Uranus"};
             
             planetList.AddRange(planetList2);
-            
+            // everytime you want to console you must forEach over the data you want printed out. 
+                planetList.Insert(1,"Venus");
+                planetList.Insert(3,"Earth");
+                planetList.Insert(8,"Pluto");
             foreach(string planetsPrinting in planetList){
-                Console.WriteLine(planetsPrinting);
+                // Console.WriteLine(planetsPrinting);
             }
-
-
+            //must print out the string of "rocky Planet" in the rocky planets list
+            List<string> rockyPlanets = planetList.GetRange(0,4);
+            foreach(string rockyPlanet in rockyPlanets){
+                Console.WriteLine(rockyPlanet);
+            }
         }
     }
 }
