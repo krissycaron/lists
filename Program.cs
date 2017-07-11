b98d9a72be6303b07c248d04dec1144a8fb86937
Kristin Caron <krissycaron@gmail.com>
2017-07-11 10:13:00 -0500

    planets are printing from step 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace list
 {
@@ -10,6 +11,15 @@ namespace list
             List<string> planetList = new List<string>(){"Mercury", "Mars"};
             planetList.Add("Jupiter");
             planetList.Add("Saturn");
+        
+            List<string> planetList2 = new List<string>(){"neptune", "uranus"};
+            
+            planetList.AddRange(planetList2);
+            
+            foreach(string planetsPrinting in planetList){
+                Console.WriteLine(planetsPrinting);
+            }
+
 
         }
     }
