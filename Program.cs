8469441d027f047e3c95797544ed571a56a1f8f5
Kristin Caron <krissycaron@gmail.com>
2017-07-11 09:55:39 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is the method list of type = string
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+        }
+    }
+}
